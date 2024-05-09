1952eb5221ac93aeb1b235bd80151e40d14ac911
Lucas Povolo <lucas.povolo1@gmail.com>
2024-05-09 18:59:28 -0300

    cambios en la toggleFunc()

--- funciones.js
+++ funciones.js
@@ -1,8 +1,16 @@
 //FUNCION DEFINIDA PARA HACER TOOGLE SEGUN SU ID, APLICANDO LA CLASE SHOW Y NOSHOW
-function toggleFunc(id) {
+  function toggleFunc(id) {
     let ele = document.getElementById(id).nextElementSibling;
+    let child = document.getElementById(id).firstElementChild;
+    console.log(child)
 
     if (ele.classList.contains("submenu-noshow")) {
       ele.classList.toggle("submenu-show");
-    }
-  }
+      child.classList.toggle("rotate");
+    } else {
+      ele.classList.toggle("submenu-noshow");
+      child.classList.toggle("rotate");
+    };
+  };
+
+  
