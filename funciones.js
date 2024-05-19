3dbc0a8d0e878498e0209030e31078aa5b5dcddd
Lucas Povolo <44147086+Ozhammar@users.noreply.github.com>
2024-05-19 17:31:24 -0300

    Revert "CORRECCIÓN DE ERROR"

--- funciones.js
+++ funciones.js
@@ -42,35 +42,4 @@ function idRandom() {
       }      
     }
   }
-
-}
-
-//FUNCION DE VALIDACION DE FORMULARIO
-function validarFormulario(event){
-  const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  let nombre = document.getElementById("nombre").value;
-  let apellido = document.getElementById("apellido").value;
-  let email = document.getElementById("email").value;
-
-  if(nombre.length < 2){
-    alert("El nombre debe contener al menos 2 caracteres.");
-    event.preventDefault();
-    return false;    
-  }
-
-  if(apellido.length < 1){
-    alert("El apellido debe contener al menos 1 caracter.");
-    event.preventDefault();
-    return false;    
-  }
-
-  if(!regex.test(email)){
-    alert("Email invalido.");
-    event.preventDefault();
-    return false;   
-  }
-
-
-  return true;
 };
-a
